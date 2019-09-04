89830d3e200219716e851f242fae553e6679a06a
adityadhiman12 <adityadhiman12@gmail.com>
2019-09-04 16:17:33 +0530

    updated index.js file

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const csv=require('csvtojson')
 const csvFilePath='./IPL-dataset/matches.csv'
+const csvFilePath_2='./IPL-dataset/deliveries.csv'
 const matchesPerYear=require('./ipl/matchesPerYear')
 
 csv()
 .fromFile(csvFilePath)
 .then((matches)=>{
     /*First Function*/
-let matchesPerYearResult = MatchesPerYear(matches);
+let matchesPerYearResult = matchesPerYear(matches);
 console.log(matchesPerYearResult)
 
 })
