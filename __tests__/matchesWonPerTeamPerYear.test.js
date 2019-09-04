ab560dc62a8d3c2188fa7644826cf605456cbcf4
adityadhiman12 <adityadhiman12@gmail.com>
2019-09-04 14:59:55 +0530

    updated test file of func(MatchesWonPerTeamPerYear)

--- __tests__/matchesWonPerTeamPerYear.test.js
+++ __tests__/matchesWonPerTeamPerYear.test.js
@@ -1,81 +1,37 @@
-const matchesWonPerTeamPerYear = require("../ipl/matchesWonPerTeamPerYear.js");
+const matchesWonPerTeamPerYear = require('../ipl/matchesWonPerTeamPerYear');
 
 test("Output of dataset with 1 entry", () => {
-  expect(
-    matchesWonPerTeamPerYear([
-      {
-        id: "110",
+    const test = 
+    [
+        {
         season: "2008",
-        city: "Coimbatore",
-        date: "2008-05-17",
-        team1: "Delhi Daredevils",
-        team2: "Kolkata Knight Riders",
-        toss_winner: "Delhi Daredevils",
-        toss_decision: "bat",
-        result: "normal",
-        dl_applied: "1",
-        winner: "Kolkata Knight Riders",
-        win_by_runs: "6",
-        win_by_wickets: "0",
-        player_of_match: "DPMD Jayawardene",
-        venue: "Feroz Shah Kotla",
-        umpire1: "AV Jayaprakash",
-        umpire2: "RE Koertzen",
-        umpire3: ""
-      }
-    ])
-  ).toEqual({"2008":{"Kolkata Knight Riders": 1 }});
+        winner: "Kolkata Knight Riders"
+        }
+    ]
+  expect(
+    matchesWonPerTeamPerYear(test)
+  ).toEqual({ "2008": { "Kolkata Knight Riders": 1 } });
 });
 
-test("Output of dataset with 2 same entries", () => {
-  expect(
-    matchesWonPerTeamPerYear([
-      {
-        id: "111",
-        season: "2009",
-        city: "Mumbai",
-        date: "2008-09-19",
-        team1: "Mumbai Indians",
-        team2: "Kolkata Knight Riders",
-        toss_winner: "Kolkata Knight Riders",
-        toss_decision: "bat",
-        result: "normal",
-        dl_applied: "1",
-        winner: "Kolkata Knight Riders",
-        win_by_runs: "6",
-        win_by_wickets: "0",
-        player_of_match: "DPMD Jayawardene",
-        venue: "Feroz Shah Kotla",
-        umpire1: "AV Jayaprakash",
-        umpire2: "RE Koertzen",
-        umpire3: ""
-      },
-      {
-        id: "110",
-        season: "2009",
-        city: "Coimbatore",
-        date: "2008-05-17",
-        team1: "Delhi Daredevils",
-        team2: "Kolkata Knight Riders",
-        toss_winner: "Delhi Daredevils",
-        toss_decision: "bat",
-        result: "normal",
-        dl_applied: "1",
-        winner: "Delhi Daredevils",
-        win_by_runs: "6",
-        win_by_wickets: "0",
-        player_of_match: "DPMD Jayawardene",
-        venue: "Feroz Shah Kotla",
-        umpire1: "AV Jayaprakash",
-        umpire2: "RE Koertzen",
-        umpire3: ""
-      }
-    ])
-  ).toEqual({ 2009:{
-    'Kolkata Knight Riders' :1,
-    'Delhi Daredevils' :1
-  }  });
-});
+// test("Output of dataset with 2 same entries", () => {
+//   expect(
+//     matchesWonPerTeamPerYear([
+//       {
+//         season: "2009",
+//         winner: "Kolkata Knight Riders"
+//       },
+//       {
+//         season: "2009",
+//         winner: "Delhi Daredevils"
+//       }
+//     ])
+//   ).toEqual({
+//     2009: {
+//       "Kolkata Knight Riders": 1,
+//       "Delhi Daredevils": 1
+//     }
+//   });
+// });
 
 // test("Output of dataset with 3 same entries", () => {
 //   expect(
